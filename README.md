e9784d3088700f91118d21e8744f3e004db41c04
Dan Miller <dsmiller95@gmail.com>
2023-09-29 15:04:45 -0500

    create a C# library project, and a unit test project attached to it.
    There doesn't appear to be any good unit testing solution for godot 4 with C#! apparently they have some of the same support problems.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Ludum Dare 54
 
-A godot 4 project. Godot project root is at ./godot
+A godot 4 project. Godot project root is at ./godot .
+The C# solution root is this directory, containing the godot scripts at /godot and supporting dotnet library projects in /dotnet/** 
 
 [project board here](https://trello.com/b/SRqaRI8K/ludum-dare-54)
 
--- godot/FirstScript.cs
+++ godot/FirstScript.cs
@@ -3,13 +3,17 @@ using System;
 
 public partial class FirstScript : Node
 {
+	private AnotherScript proxy = new AnotherScript();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		proxy._Ready();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		proxy._Process(delta);
+		GD.Print(proxy.Time);
 	}
 }
