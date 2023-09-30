95a48217966aa941595b184b3f96a0b44fa20211
Brandon <brandon.j.salmon@gmail.com>
2023-09-29 23:29:33 -0500

    Player and Audience stubs

---
+++ godot/Player/Player.cs
@@ -0,0 +1,45 @@
+using Godot;
+using System;
+
+public partial class Player : RigidBody2D
+{
+	[Export]
+	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var velocity = Vector2.Zero; // The player's movement vector.
+
+		if (Input.IsActionPressed("move_right"))
+		{
+			velocity.X += 1;
+		}
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			velocity.X -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			velocity.Y += 1;
+		}
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			velocity.Y -= 1;
+		}
+
+		if (velocity.Length() > 0)
+		{
+			velocity = velocity.Normalized() * Speed;
+			Position += velocity * (float)delta;
+		}
+	}
+}
