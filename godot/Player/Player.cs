59095f8e6164fc4946555a118f653e4a64ce798a
dsmiller95 <dsmiller95@gmail.com>
2023-09-30 11:36:44 -0500

    delete the FMOD extension, it is malformed

--- godot/Player/Player.cs
+++ godot/Player/Player.cs
@@ -5,6 +5,7 @@ public partial class Player : RigidBody2D
 {
 	[Export] public int AccelerationForce { get; set; } = 400; // How fast the player will accelerate (pixels/sec^2).
 	[Export] public int MaximumVelocity { get; set; } = 400; // the max velocity the player will move (pixels/sec).
+	[Export] public int ActiveFrictionCoefficient { get; set; } = 10; // Resistance to movement. force / velocity (kg/s)
 	
 	private Vector2 GetInputVectorNormalized()
 	{
@@ -36,9 +37,12 @@ public partial class Player : RigidBody2D
 
 	public override void _IntegrateForces(PhysicsDirectBodyState2D state)
 	{
-		var input = GetInputVectorNormalized(); 
+		var input = GetInputVectorNormalized();
+		var inputAcceleration = input * AccelerationForce;
 		
-		state.ApplyCentralForce(input * AccelerationForce);
+		var activeFriction = state.LinearVelocity * -ActiveFrictionCoefficient;
+		
+		state.ApplyCentralForce(inputAcceleration + activeFriction);
 
 		var velocityMagnitude = state.LinearVelocity.Length();
 		if (velocityMagnitude > MaximumVelocity)
@@ -46,6 +50,5 @@ public partial class Player : RigidBody2D
 			velocityMagnitude = MaximumVelocity;
 			state.LinearVelocity = state.LinearVelocity.Normalized() * velocityMagnitude;
 		}
-		
 	}
 }
