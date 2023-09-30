3afe3d66203e88e4bf62e7bf754ef2d38cedf10e
Brandon <brandon.j.salmon@gmail.com>
2023-09-29 23:34:35 -0500

    Collide

--- godot/Player/Player.cs
+++ godot/Player/Player.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Player : RigidBody2D
+public partial class Player : CharacterBody2D
 {
 	[Export]
 	public int Speed { get; set; } = 400; // How fast the player will move (pixels/sec).
