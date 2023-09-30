2c8cbd8127246f4b7891e49c9be60543cd0e54e2
dsmiller95 <dsmiller95@gmail.com>
2023-09-30 12:41:24 -0500

    make the player an oval
    implement a shimmy mechanic. pressing space will rotate the player, and lock that rotation regarless of how they move
    
    make the attendees oval
    
    make the physics calculation for person movement a bit more generic. might want to use this for the npcs too.

--- godot/Player/Player.cs
+++ godot/Player/Player.cs
@@ -1,11 +1,15 @@
 using Godot;
 using System;
+using DotnetLibrary;
 
 public partial class Player : RigidBody2D
 {
 	[Export] public int AccelerationForce { get; set; } = 400; // How fast the player will accelerate (pixels/sec^2).
+	[Export] public int RotationalAcceleration { get; set; } = 400; // How much force will apply to keep the player facing forward (kg pixels^2 / sec^2 radians) aka (Torque / radian)
 	[Export] public int MaximumVelocity { get; set; } = 400; // the max velocity the player will move (pixels/sec).
 	[Export] public int ActiveFrictionCoefficient { get; set; } = 10; // Resistance to movement. force / velocity (kg/s)
+
+	private Vector2? lastTurnInput = null;
 	
 	private Vector2 GetInputVectorNormalized()
 	{
@@ -35,20 +39,49 @@ public partial class Player : RigidBody2D
 		return velocity;
 	}
 
+	private bool IsInputTurnedToSide()
+	{
+		return Input.IsActionPressed("turn_side");
+	}
+
+	private Vector2? DesiredForwardDirection(Vector2 input)
+	{
+		if (input.Length() > 0.01 && (!lastTurnInput.HasValue || !IsInputTurnedToSide()))
+		{
+			lastTurnInput = input;
+		}
+
+		if (!lastTurnInput.HasValue) return null;
+		
+		var targetForward = lastTurnInput.Value;
+		if (IsInputTurnedToSide())
+		{
+			targetForward = targetForward.Rotated(Mathf.Pi / 2);
+		}
+
+		return targetForward;
+	}
+
 	public override void _IntegrateForces(PhysicsDirectBodyState2D state)
 	{
-		var input = GetInputVectorNormalized();
-		var inputAcceleration = input * AccelerationForce;
-		
-		var activeFriction = state.LinearVelocity * -ActiveFrictionCoefficient;
-		
-		state.ApplyCentralForce(inputAcceleration + activeFriction);
-
-		var velocityMagnitude = state.LinearVelocity.Length();
-		if (velocityMagnitude > MaximumVelocity)
+		var myPhysics = new PersonPhysics
 		{
-			velocityMagnitude = MaximumVelocity;
-			state.LinearVelocity = state.LinearVelocity.Normalized() * velocityMagnitude;
-		}
+			AccelerationForce = AccelerationForce,
+			RotationalAcceleration = RotationalAcceleration,
+			MaximumVelocity = MaximumVelocity,
+			ActiveFrictionCoefficient = ActiveFrictionCoefficient
+		};
+		
+		var input = GetInputVectorNormalized();
+		var desiredLinearForce = input * AccelerationForce;
+		var desiredLookDirection = DesiredForwardDirection(input);
+
+		var integrationResult = myPhysics.GetLinearForce(
+			desiredLinearForce,
+			desiredLookDirection,
+			state.LinearVelocity,
+			state.Transform.X);
+		
+		integrationResult.ApplyTo(state);
 	}
 }
