a31e011fcde7f9cec6f339ade445e80e2e4eed14
Dan Miller <dsmiller95@gmail.com>
2023-09-29 23:11:23 -0500

    add a basic halton sequence spawner script

---
+++ dotnet/Library/HaltonSequence.cs
@@ -0,0 +1,71 @@
+using Godot;
+
+namespace DotnetLibrary;
+
+/// <summary>
+/// An utility class to compute samples on the Halton sequence.
+/// https://en.wikipedia.org/wiki/Halton_sequence
+/// </summary>
+public static class HaltonSequence
+{
+    /// <summary>
+    /// Gets a deterministic sample in the Halton sequence.
+    /// </summary>
+    /// <param name="index">The index in the sequence.</param>
+    /// <param name="radix">The radix of the sequence.</param>
+    /// <returns>A sample from the Halton sequence.</returns>
+    public static float Get(int index, int radix)
+    {
+        float result = 0f;
+        float fraction = 1f / radix;
+
+        while (index > 0)
+        {
+            result += (index % radix) * fraction;
+
+            index /= radix;
+            fraction /= radix;
+        }
+
+        return result;
+    }
+
+    public static IEnumerable<Vector2> SampleVector2Field(
+        int horizontalRadix,
+        int verticalRadix,
+        int seed, 
+        Vector2 maxVector,
+        Vector2 minVector,
+        int maxSamples = 10000)
+    {
+        var indexInSequence = seed;
+        var scale                = maxVector - minVector;
+
+        var i = 0;
+        while (i++ < maxSamples)
+        {
+            var sample = new Vector2(
+                Get(indexInSequence, horizontalRadix),
+                Get(indexInSequence, verticalRadix)
+            );
+            indexInSequence++;
+
+            yield return sample * scale + minVector;
+        }
+    }
+}
+
+public class SampleHaltonPoints : ISamplePoints
+{
+    public IEnumerable<Vector2> SampleVector2Field(
+        int seed,
+        Rect2 spawnArea)
+    {
+        return HaltonSequence.SampleVector2Field(
+            2,
+            3,
+            seed,
+            spawnArea.End,
+            spawnArea.Position);
+    }
+}
---
+++ godot/RandomSpawner.cs
@@ -0,0 +1,29 @@
+using System.Linq;
+using DotnetLibrary;
+using Godot;
+
+namespace LudumDare54;
+
+public partial class RandomSpawner : Node2D
+{
+    [Export] private Node2D ParentNode { get; set; }
+    [Export] private PackedScene SceneToSpawn { get; set; }
+
+    [Export] private Rect2 SpawnArea { get; set; } = new Rect2(10, 10, 500, 500);
+    [Export] private int SpawnNum { get; set; } = 100;
+    
+    public override void _Ready()
+    {
+        var rng = new RandomNumberGenerator();
+        rng.Randomize();
+        ISamplePoints pointSampler = new SampleHaltonPoints();
+        foreach (var nextPoint in pointSampler
+                     .SampleVector2Field(rng.RandiRange(1, int.MaxValue), SpawnArea)
+                     .Take(SpawnNum))
+        {
+            var instance = SceneToSpawn.Instantiate<Node2D>();
+            instance.GlobalPosition = this.ToGlobal(nextPoint);
+            ParentNode.AddChild(instance);
+        }
+    }
+}
