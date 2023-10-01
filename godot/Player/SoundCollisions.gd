cf15f483b501d98955c717739089397ff02f4bac
Spike Razer <sam17pike@gmail.com>
2023-10-01 14:56:10 -0500

    tweaked how soundCollisions.gd works to use Brandon's new signals and added an event for Cullen's guitar shredding

--- godot/Player/SoundCollisions.gd
+++ godot/Player/SoundCollisions.gd
@@ -7,12 +7,18 @@ var instance: EventInstance
 
 # Called when the node enters the scene tree for the first time.
 func _enter_tree() -> void:
-	body.body_entered.connect(_play_one_shot)
+	body.connect("SoftCollision", _play_soft_collision)
 
 func _exit_tree()-> void:
-	body.body_entered.disconnect(_play_one_shot)
+	body.disconnect("SoftCollision", _play_soft_collision)
+		
+func _play_soft_collision():
+	instance = FMODRuntime.create_instance(event)
+	instance.start()
+	instance.release()
 	
-func _play_one_shot(body):
+		
+func _play_hard_collision():
 	instance = FMODRuntime.create_instance(event)
 	instance.start()
 	instance.release()
