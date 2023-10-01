1c8cdbc4a15a047dce0b2fb064796ebb48ed95e1
Spike Razer <sam17pike@gmail.com>
2023-10-01 16:54:58 -0500

    first spill effect implemented and some other sound adjustments

--- godot/Player/SoundCollisions.gd
+++ godot/Player/SoundCollisions.gd
@@ -2,23 +2,25 @@ extends Node
 
 @export var body: RigidBody2D 
 
-@export var event: EventAsset
+@export var bumpEffect: EventAsset
+@export var spillEffect: EventAsset
 var instance: EventInstance
 
 # Called when the node enters the scene tree for the first time.
 func _enter_tree() -> void:
 	body.connect("SoftCollision", _play_soft_collision)
+	body.connect("HealthDepleted", _play_hard_collision)
 
 func _exit_tree()-> void:
 	body.disconnect("SoftCollision", _play_soft_collision)
+	body.disconnect("HealthDepleted", _play_hard_collision)
 		
 func _play_soft_collision():
-	instance = FMODRuntime.create_instance(event)
+	instance = FMODRuntime.create_instance(bumpEffect)
 	instance.start()
 	instance.release()
-	
 		
 func _play_hard_collision():
-	instance = FMODRuntime.create_instance(event)
+	instance = FMODRuntime.create_instance(spillEffect)
 	instance.start()
 	instance.release()
