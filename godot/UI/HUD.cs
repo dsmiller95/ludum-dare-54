8e4e7cb306f1c39176fd20a495f3afa0a6489245
quitedrifty <138937791+quitedrifty@users.noreply.github.com>
2023-10-01 16:36:34 -0500

    added arrow sprite to pointer

--- godot/UI/HUD.cs
+++ godot/UI/HUD.cs
@@ -11,7 +11,7 @@ public partial class HUD : CanvasLayer
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		sprite = (Sprite2D)GetNode("Pointer");
+		sprite = (Sprite2D)GetNode("Pointer").GetNode("PointerSprite");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
