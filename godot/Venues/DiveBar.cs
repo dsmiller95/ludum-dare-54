02723770ae8669a8d5c44846bf2e6b972f3173ee
ludumdare <quitedrifty@gmail.com>
2023-09-30 19:41:37 -0500

    Added goal and spinny sprite to point towards goal.

---
+++ godot/Venues/DiveBar.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class DiveBar : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+
+
+	private void _on_goal_body_entered(Node2D body)
+	{
+		if (body.Name == "Player")
+		{
+			GD.Print("Level Complete");
+		}
+	}
+
+}
