4b7238e8231c1f7ecd1d29277dcb1a9eb8904bd9
Dan Miller <dsmiller95@gmail.com>
2023-10-02 12:35:21 -0500

    build to Mosh Pint.exe

--- build_and_run_win.sh
+++ build_and_run_win.sh
@@ -3,4 +3,4 @@ mkdir -p build/win/
 
 "F:\PortablePrograms\Godot\godot4_console.exe"  --path godot --export-release "Windows Desktop"
 
-"build/win/Ludum Dare 54.exe" --path "build/win"
+"build/win/Mosh Pint.exe" --path "build/win"
