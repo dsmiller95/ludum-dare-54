c52f127f654c39f03d845076bc6811b5d807b243
Dan Miller <dsmiller95@gmail.com>
2023-10-02 10:40:23 -0500

    don't use integrate forces for player, we can do it all in physics update safely.

--- godot/Player/Player.cs
+++ godot/Player/Player.cs
@@ -13,6 +13,7 @@ public partial class Player : RigidBody2D, IHavePersonBody
 	[Export] public PersonPhysicsDefinition PersonMovement { get; set; } = null!;
 
 	private Vector2? lastTurnInput = null;
+	private float currentSturdyFactor = 0f;
 
 	private PersonBody personBody;
 
@@ -30,8 +31,47 @@ public partial class Player : RigidBody2D, IHavePersonBody
 	public override void _PhysicsProcess(double delta)
 	{
 		personBody._PhysicsProcess();
+		var myPhysics = PersonMovement.GetConfiguredPhysics();
+
+		var input = GetInputVectorNormalized();
+		var desiredLinearForce = input * AccelerationForce;
+		var desiredLookDirection = DesiredForwardDirection(input);
+
+		var integrationResult = myPhysics.ComputeIntegrationResult(
+			desiredLinearForce,
+			desiredLookDirection,
+			this.LinearVelocity,
+			this.Transform.X);
+
+		integrationResult.ApplyTo(this);
 	}
 
+	public void OnBodyEntered(Node bodyGeneric)
+	{
+		if (bodyGeneric is not RigidBody2D otherBody) return;
+		
+		var impactVector = otherBody.LinearVelocity - LinearVelocity;
+		var impact = impactVector.Length();
+		if (!(impact > 100))
+		{
+			EmitSignal("SoftCollision");
+			return;
+		}
+
+		Health.AdjustHealth(-5);
+		EmitSignal("HealthDepleted");
+		var spill = GetNode<CpuParticles2D>("SpillParticles");
+		spill.Direction = new Vector2(impactVector.Y, -impactVector.X);
+		spill.InitialVelocityMin = impact;
+		spill.InitialVelocityMax = impact * 5;
+		spill.Emitting = true;
+
+		if (Health.HealthValue > 0) return;
+		
+		EmitSignal("YouDied");
+		GetTree().ChangeSceneToFile("res://MenuScenes/GameOverScene.tscn");
+	}
+	
 	private Vector2 GetInputVectorNormalized()
 	{
 		var velocity = Vector2.Zero; // The player's movement vector.
@@ -85,49 +125,4 @@ public partial class Player : RigidBody2D, IHavePersonBody
 		return targetForward;
 	}
 
-	public override void _IntegrateForces(PhysicsDirectBodyState2D state)
-	{
-		var myPhysics = PersonMovement.GetConfiguredPhysics();
-
-		var input = GetInputVectorNormalized();
-		var desiredLinearForce = input * AccelerationForce;
-		var desiredLookDirection = DesiredForwardDirection(input);
-
-		var integrationResult = myPhysics.ComputeIntegrationResult(
-			desiredLinearForce,
-			desiredLookDirection,
-			state.LinearVelocity,
-			state.Transform.X);
-
-		integrationResult.ApplyTo(state);
-	}
-
-	public void OnBodyEntered(Node bodyGeneric)
-	{
-		if (bodyGeneric is RigidBody2D otherBody)
-		{
-			var impactVector = otherBody.LinearVelocity - LinearVelocity;
-			var impact = impactVector.Length();
-			if (impact > 100)
-			{
-				Health.AdjustHealth(-5);
-				EmitSignal("HealthDepleted");
-				var spill = GetNode<CpuParticles2D>("SpillParticles");
-				spill.Direction = new Vector2(impactVector.Y, -impactVector.X);
-				spill.InitialVelocityMin = impact;
-				spill.InitialVelocityMax = impact * 5;
-				spill.Emitting = true;
-
-				if (Health.HealthValue <= 0)
-				{
-					EmitSignal("YouDied");
-					GetTree().ChangeSceneToFile("res://MenuScenes/GameOverScene.tscn");
-				}
-			}
-			else
-			{
-				EmitSignal("SoftCollision");
-			}
-		}
-	}
 }
