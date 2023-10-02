98c31e7f1328148ba7458060b3d1d2fadab2ca77
Dan Miller <dsmiller95@gmail.com>
2023-10-02 01:13:04 -0500

    add a wildcard factor

--- godot/Audience/FactorArchetype.cs
+++ godot/Audience/FactorArchetype.cs
@@ -7,6 +7,7 @@ namespace LudumDare54.Audience;
 [GlobalClass]
 public partial class FactorArchetype : Resource, IProvideFactorAccumulation
 {
+    [Export] public float RandomMultiplier = 0.1f;
     [Export] public float GlobalMultiplier = 1f;
     [Export] public float RagePerSecond = 0f;
     [Export] public float StuporPerSecond = 0f;
@@ -16,6 +17,14 @@ public partial class FactorArchetype : Resource, IProvideFactorAccumulation
     
     public FactorAccumulation GetAccumulation()
     {
+        var randomAccumulation = new FactorAccumulation(0f);
+        randomAccumulation.AccumulateFactor(FactorType.Rage, GD.Randf());
+        randomAccumulation.AccumulateFactor(FactorType.Stupor, GD.Randf());
+        randomAccumulation.AccumulateFactor(FactorType.StinkyToAttractive, GD.Randf());
+        randomAccumulation.AccumulateFactor(FactorType.Horny, GD.Randf());
+        randomAccumulation.AccumulateFactor(FactorType.IntrovertToExtrovert, GD.Randf());
+        randomAccumulation.NormalizeAllAccumulates(RandomMultiplier);
+        
         var accumulation = new FactorAccumulation(0f);
         accumulation.AccumulateFactor(FactorType.Rage, RagePerSecond);
         accumulation.AccumulateFactor(FactorType.Stupor, StuporPerSecond);
@@ -23,6 +32,10 @@ public partial class FactorArchetype : Resource, IProvideFactorAccumulation
         accumulation.AccumulateFactor(FactorType.Horny, HornyPerSecond);
         accumulation.AccumulateFactor(FactorType.IntrovertToExtrovert, IntrovertToExtrovertPerSecond);
         accumulation.NormalizeAllAccumulates(GlobalMultiplier);
+        
+        
+        accumulation.AddAll(randomAccumulation);
+        
         return accumulation;
     }
 }
