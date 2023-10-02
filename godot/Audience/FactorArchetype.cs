d56125247f3c8df0f8f182bd3731e4c34fb0baa7
dsmiller95 <dsmiller95@gmail.com>
2023-10-01 19:47:39 -0500

    this tuning looks good in the ai test scene. abysmall performance though.

--- godot/Audience/FactorArchetype.cs
+++ godot/Audience/FactorArchetype.cs
@@ -7,6 +7,7 @@ namespace LudumDare54.Audience;
 [GlobalClass]
 public partial class FactorArchetype : Resource, IProvideFactorAccumulation
 {
+    [Export] public float GlobalMultiplier = 1f;
     [Export] public float RagePerSecond = 0f;
     [Export] public float StuporPerSecond = 0f;
     [Export] public float StinkyToAttractivePerSecond = 0f;
@@ -16,11 +17,11 @@ public partial class FactorArchetype : Resource, IProvideFactorAccumulation
     public FactorAccumulation GetAccumulation()
     {
         var accumulation = new FactorAccumulation(0f);
-        accumulation.AccumulateFactor(FactorType.Rage, RagePerSecond);
-        accumulation.AccumulateFactor(FactorType.Stupor, StuporPerSecond);
-        accumulation.AccumulateFactor(FactorType.StinkyToAttractive, StinkyToAttractivePerSecond);
-        accumulation.AccumulateFactor(FactorType.Horny, HornyPerSecond);
-        accumulation.AccumulateFactor(FactorType.IntrovertToExtrovert, IntrovertToExtrovertPerSecond);
+        accumulation.AccumulateFactor(FactorType.Rage, RagePerSecond * GlobalMultiplier);
+        accumulation.AccumulateFactor(FactorType.Stupor, StuporPerSecond * GlobalMultiplier);
+        accumulation.AccumulateFactor(FactorType.StinkyToAttractive, StinkyToAttractivePerSecond * GlobalMultiplier);
+        accumulation.AccumulateFactor(FactorType.Horny, HornyPerSecond * GlobalMultiplier);
+        accumulation.AccumulateFactor(FactorType.IntrovertToExtrovert, IntrovertToExtrovertPerSecond * GlobalMultiplier);
         return accumulation;
     }
 }
